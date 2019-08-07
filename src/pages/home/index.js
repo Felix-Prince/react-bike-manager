eea0dd1c92af22f7b7f00137735e0e30a7b7e6ee
Felix-Prince <zhupengfei199@gmail.com>
2019-08-07 20:26:14 +0800

    登录bug修复

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,7 @@
-import React from 'react'
-import './index.less'
-export default class Home extends React.Component{
-
-    render(){
-        return (
-            <div className="home-wrap">
-                欢迎学习IMooc后台管理系统课程
-            </div>
-        );
+import React from "react"
+import "./index.less"
+export default class Home extends React.Component {
+    render() {
+        return <div className="home-wrap">欢迎学习Felix后台管理系统课程</div>
     }
 }
